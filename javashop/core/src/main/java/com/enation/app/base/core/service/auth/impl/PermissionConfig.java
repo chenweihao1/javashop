36c6b0347d581e2ee18bd66db7378d24fecd10e9
SylowFeng <sylow@javashop.cn>
2018-09-18 13:22:16 +0800

    修改pom使资源文件正确加载

--- javashop/core/src/main/java/com/enation/app/base/core/service/auth/impl/PermissionConfig.java
+++ javashop/core/src/main/java/com/enation/app/base/core/service/auth/impl/PermissionConfig.java
@@ -10,13 +10,13 @@ import com.enation.framework.util.StringUtil;
 
 public  class PermissionConfig {
 	private static Map<String,Integer> authMap= new HashMap<String,Integer>();
-	static{ 
- 
+	static{
+
 		try{
 			InputStream in  = FileUtil.getResourceAsStream("auth.properties");
 			Properties props = new Properties();
 			props.load(in);
-				
+
 			int super_admin= StringUtil.toInt(props.getProperty("auth.super_admin"), true);
 			int goods = StringUtil.toInt(props.getProperty("auth.goods"), true);
 			int order = StringUtil.toInt(props.getProperty("auth.order"), true);
@@ -24,19 +24,19 @@ public  class PermissionConfig {
 			int finance = StringUtil.toInt(props.getProperty("auth.finance"), true);
 			int customer_service = StringUtil.toInt(props.getProperty("auth.customer_service"), true);
 			int depot_ship = StringUtil.toInt(props.getProperty("auth.depot_ship"), true);
-	
+
 			authMap.put("super_admin", super_admin); //超级管理员
 			authMap.put("goods", goods); //商品管理权限
 			authMap.put("order", order); //订单管理权限
 			authMap.put("depot_admin", depot_admin); //库管
 			authMap.put("finance", finance);//财务
 			authMap.put("customer_service", customer_service); //客服权限
-			
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static int getAuthId(String type){
 		return authMap.get(type);
 	}
